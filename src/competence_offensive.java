967d82ddbe5c09f09b1b64eb60702e6b316de3b7
Unknown <johann.feschet@gmail.com>
2017-12-02 17:36:53 +0100

    comp

--- src/competence_offensive.java
+++ src/competence_offensive.java
@@ -3,7 +3,7 @@ public abstract class competence_offensive extends Competence {
 	
 	private int degat;
 	private int degat_tour;
-	private int statut;//1=poison,2=stun,3=
+	private int statut;//1=poison,2=stun,
 	
 	
 
